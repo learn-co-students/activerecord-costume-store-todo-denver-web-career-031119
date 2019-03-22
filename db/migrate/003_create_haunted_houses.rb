79d74bf378c20facc415981782910ea650409cc3
ibenthinkin <benjamin.reilly@gmail.com>
2019-03-22 15:06:54 -0600

    Built the lab

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,29 @@
 # Create your haunted_houses migration here
+
+
+class CreateHauntedHouses < ActiveRecord::Migration[4.2]
+  def change
+    create_table :haunted_houses do |c|
+      c.text :name
+      c.text :location
+      c.text :theme
+      c.float :price
+      c.text :family_friendly
+      c.date :opening_date
+      c.date :closing_date
+      c.text :description
+
+    end
+  end
+
+end
+
+# HauntedHouse
+#   has a name (FAILED - 1)
+#   has a location (FAILED - 2)
+#   has a theme (FAILED - 3)
+#   has a price (FAILED - 4)
+#   knows if it's family friendly (FAILED - 5)
+#   has an opening date (FAILED - 6)
+#   has a closing date (FAILED - 7)
+#   has a long, long description (FAILED - 8)
